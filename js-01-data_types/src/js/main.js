bd2b6056724a1864e82d136e8cb8de7305cb92b0
Sergio Torres <sergio.torres@idr.edu.mx>
2025-11-26 10:53:09 -0600

    feat: uso de string template literals

--- js-01-data_types/src/js/main.js
+++ js-01-data_types/src/js/main.js
@@ -12,3 +12,19 @@ console.log("Session JS01 - Data Types");
    - Correcto: userAge, firstName, isActive
    - Incorrecto: user_age, FirstName, is_active
 */
+
+// TODO: Declara 3 variables para un perfil de usuario ficticio
+const userName = "John Doe"; // String
+let userAge = 30; // Number
+let isMember = true; // Boolean
+
+// --- Template Literals (Backticks ``) ---
+// Permiten interpolación ${} y saltos de línea. Más limpio que concatenar con '+'.
+// ejemplo:  `Hola ch62, soy ${userName}`
+
+const firstName = "Daniel";
+let currentAge = 24;
+const currentNationality = "Mexican";
+
+// De las variables anteriores muestra en console el sig. mensaje:
+// "My name is Daniel, I am 24 years old and I am Mexican."
